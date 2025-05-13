76ff527e63df11296f35fad96fb96f6fd65264fc
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-05-13 23:21:25 +0900

    feat: 합동세미나 모바일 앱 6팀 초기 세팅

---
+++ src/main/java/org/sopt36th/seminar/domain/contract/domain/Deposit.java
@@ -0,0 +1,8 @@
+package org.sopt36th.seminar.domain.contract.domain;
+
+import jakarta.persistence.Entity;
+import org.sopt36th.seminar.common.entity.BaseEntity;
+
+@Entity
+public class Deposit extends BaseEntity {
+}
---
+++ src/main/java/org/sopt36th/seminar/domain/saving/domain/PreferentialRate.java
@@ -0,0 +1,8 @@
+package org.sopt36th.seminar.domain.saving.domain;
+
+import jakarta.persistence.Entity;
+import org.sopt36th.seminar.common.entity.BaseEntity;
+
+@Entity
+public class PreferentialRate extends BaseEntity {
+}
