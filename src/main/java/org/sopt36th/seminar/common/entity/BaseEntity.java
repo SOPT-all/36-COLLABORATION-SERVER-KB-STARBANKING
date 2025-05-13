440360dcf5550c8160f640d7efac93871d7a89ff
kaswhy <sxshx818@naver.com>
2025-05-13 23:21:35 +0900

    fix(common): BaseEntity ID를 Identity 전략으로 변경

--- src/main/java/org/sopt36th/seminar/common/entity/BaseEntity.java
+++ src/main/java/org/sopt36th/seminar/common/entity/BaseEntity.java
@@ -2,6 +2,8 @@ package org.sopt36th.seminar.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
@@ -16,6 +18,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @CreatedDate
