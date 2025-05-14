f7a7207605a4a96ecd2b9633e51afda48ff34043
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-05-14 23:37:25 +0900

    [feat/#2] Entity 구현

--- src/main/java/org/sopt36th/seminar/domain/Contract.java
+++ src/main/java/org/sopt36th/seminar/domain/Contract.java
@@ -1,9 +1,91 @@
 package org.sopt36th.seminar.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import org.sopt36th.seminar.common.entity.BaseEntity;
+import org.sopt36th.seminar.domain.enums.ContractState;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Contract extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "saving_id", nullable = false)
+    private Saving saving;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "contract", cascade = CascadeType.ALL)
+    private List<Deposit> deposits = new ArrayList<>();
+
+    @Column(length = 30, nullable = false)
+    private String account;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private Integer totalBalance;
+
+    private Double totalPreferentialRate;
+
+    @Enumerated(EnumType.STRING)
+    private ContractState state;
+
+    protected Contract() {
+
+    }
+
+    public Contract(Saving saving, User user, String account, LocalDate startDate, LocalDate endDate, Integer totalBalance, Double totalPreferentialRate, ContractState state) {
+        this.saving = saving;
+        this.user = user;
+        this.account = account;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.totalBalance = totalBalance;
+        this.totalPreferentialRate = totalPreferentialRate;
+        this.state = ContractState.ACTIVE;
+    }
+
+    //getter
+    public Saving getSaving() {
+        return saving;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public Integer getTotalBalance() {
+        return totalBalance;
+    }
+
+    public Double getTotalPreferentialRate() {
+        return totalPreferentialRate;
+    }
+
+    public ContractState getState() {
+        return state;
+    }
+
+    //update
+    public void updateTotalBalance(int newBalance) {
+        this.totalBalance = newBalance;
+    }
 
 }
--- src/main/java/org/sopt36th/seminar/domain/Deposit.java
+++ src/main/java/org/sopt36th/seminar/domain/Deposit.java
@@ -1,8 +1,71 @@
 package org.sopt36th.seminar.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import org.sopt36th.seminar.common.entity.BaseEntity;
+
+import java.time.LocalDate;
 
 @Entity
 public class Deposit extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "contract_id", nullable = false)
+    private Contract contract;
+
+    private LocalDate date;
+
+    private Integer afterDeposit;
+
+    private Integer payment;
+
+    private Integer count;
+
+    private Double appliedRate;
+
+    protected Deposit() {
+
+    }
+
+
+    public Deposit(Contract contract, LocalDate date, Integer afterDeposit, Integer payment, Integer count, Double appliedRate) {
+        this.contract = contract;
+        this.date = date;
+        this.afterDeposit = afterDeposit;
+        this.payment = payment;
+        this.count = count;
+        this.appliedRate = appliedRate;
+    }
+
+    //getter
+    public Contract getContract() {
+        return contract;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Integer getAfterDeposit() {
+        return afterDeposit;
+    }
+
+    public Integer getPayment() {
+        return payment;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public Double getAppliedRate() {
+        return appliedRate;
+    }
+
+    //update
+    public void updateAfterDeposit(int newBalance) {
+        this.afterDeposit = newBalance;
+    }
 }
