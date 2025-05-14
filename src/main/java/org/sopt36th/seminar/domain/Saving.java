35bf992210ee7d75672e7219fd4107ecc6e45513
kaswhy <sxshx818@naver.com>
2025-05-14 23:48:56 +0900

    [feat/#3] Saving 엔티티 작성

--- src/main/java/org/sopt36th/seminar/domain/Saving.java
+++ src/main/java/org/sopt36th/seminar/domain/Saving.java
@@ -1,9 +1,32 @@
 package org.sopt36th.seminar.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import jakarta.persistence.Entity;
 import org.sopt36th.seminar.common.entity.BaseEntity;
 
 @Entity
 public class Saving extends BaseEntity {
 
+    private String name;
+
+    @JsonFormat(shape = Shape.STRING, pattern = "0.00")
+    private Double baseRate;
+
+    protected Saving() {
+
+    }
+
+    public Saving(String name, double baseRate) {
+        this.name = name;
+        this.baseRate = baseRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getBaseRate() {
+        return baseRate;
+    }
 }
