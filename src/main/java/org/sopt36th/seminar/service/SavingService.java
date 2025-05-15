4079253d8bae324e57a8aaafe2a94c9ac6906741
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-05-16 03:02:23 +0900

    [add/#7] 어노테이션 추가

--- src/main/java/org/sopt36th/seminar/service/SavingService.java
+++ src/main/java/org/sopt36th/seminar/service/SavingService.java
@@ -1,8 +1,10 @@
 package org.sopt36th.seminar.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SavingService {
     // 효은
 
