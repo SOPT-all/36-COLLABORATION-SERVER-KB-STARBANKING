0f63b29cf05e7aec5ffc164aca7245f6c9c4c511
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-05-15 14:11:13 +0900

    [mod/#2] enum class 수정

--- src/main/java/org/sopt36th/seminar/domain/enums/ContractState.java
+++ src/main/java/org/sopt36th/seminar/domain/enums/ContractState.java
@@ -2,6 +2,16 @@ package org.sopt36th.seminar.domain.enums;
 
 
 public enum ContractState {
-    ACTIVE,
-    INACTIVE,
+    ACTIVE("정상"),
+    INACTIVE("사용 불가");
+
+    private final String state;
+
+    ContractState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
