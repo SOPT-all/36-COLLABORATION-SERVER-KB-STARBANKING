04ab5be15331f1722791d04607ff2d1071ca11c8
kaswhy <sxshx818@naver.com>
2025-05-15 20:56:20 +0900

    [add/#3] repository 스켈레톤 코드 추가

---
+++ src/main/java/org/sopt36th/seminar/repository/ContractRepository.java
@@ -0,0 +1,14 @@
+package org.sopt36th.seminar.repository;
+
+import org.sopt36th.seminar.domain.Contract;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContractRepository extends JpaRepository<Contract, Long> {
+    // 효은
+
+
+    // 소연
+
+}
---
+++ src/main/java/org/sopt36th/seminar/repository/PreferentialRateRepository.java
@@ -0,0 +1,14 @@
+package org.sopt36th.seminar.repository;
+
+import org.sopt36th.seminar.domain.PreferentialRate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PreferentialRateRepository extends JpaRepository<PreferentialRate, Long> {
+    // 효은
+
+
+    // 소연
+
+}
