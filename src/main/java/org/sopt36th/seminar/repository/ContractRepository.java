17f357626a9305af88f2986578ee32bbd7d6a063
Hyoeun <joohyoeun@sookmyung.ac.kr>
2025-05-19 15:10:16 +0900

    [feat/#6] Repository 함수 구현

--- src/main/java/org/sopt36th/seminar/repository/ContractRepository.java
+++ src/main/java/org/sopt36th/seminar/repository/ContractRepository.java
@@ -5,15 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Long> {
 
     List<Contract> findByUserId(Long userId);
 
-    List<Contract> findBySavingId(Long savingId);
-
     // 효은
+    Optional<Contract> findBySavingId(Long savingId);
 
 
     // 소연
