6e008fd20f89fcb39ec147a7481e6400ad546a38
kaswhy <sxshx818@naver.com>
2025-05-19 22:24:04 +0900

    [refactor/#16] util 사용하는 것으로 코드 변경

--- src/main/java/org/sopt36th/seminar/service/ContractService.java
+++ src/main/java/org/sopt36th/seminar/service/ContractService.java
@@ -1,14 +1,14 @@
 package org.sopt36th.seminar.service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.sopt36th.seminar.common.utils.DateUtil;
+import org.sopt36th.seminar.common.utils.TimeUtil;
 import org.sopt36th.seminar.domain.Contract;
 import org.sopt36th.seminar.dto.response.AccountResponse;
 import org.sopt36th.seminar.dto.response.GetAllAccountsResponse;
 import org.sopt36th.seminar.common.exception.custom.ContractNotFoundException;
-import org.sopt36th.seminar.domain.Contract;
 import org.sopt36th.seminar.domain.Deposit;
 import org.sopt36th.seminar.domain.Saving;
 import org.sopt36th.seminar.dto.response.GetContractDetailResponse;
@@ -18,10 +18,7 @@ import org.sopt36th.seminar.mapper.ContractStateMapper;
 import org.sopt36th.seminar.repository.ContractRepository;
 import org.sopt36th.seminar.repository.DepositRepository;
 import org.sopt36th.seminar.repository.PreferentialRateRepository;
-import org.sopt36th.seminar.repository.SavingRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 import static org.sopt36th.seminar.common.exception.GlobalErrorCode.CONTRACT_NOT_FOUND;
 
@@ -29,12 +26,8 @@ import static org.sopt36th.seminar.common.exception.GlobalErrorCode.CONTRACT_NOT
 @RequiredArgsConstructor
 public class ContractService {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-
-
     private final PreferentialRateRepository preferentialRateRepository;
     private final ContractRepository contractRepository;
-    private final SavingRepository savingRepository;
     private final DepositRepository depositRepository;
 
     // 효은
@@ -71,24 +64,13 @@ public class ContractService {
                         contract.getId(),
                         contract.getSaving().getName(),
                         contract.getAccount(),
-                        formatDate(contract.getStartDate()),
-                        formatDate(contract.getEndDate()),
-                        calculateDDay(contract.getEndDate()),
+                        TimeUtil.toFormattedTime(contract.getStartDate()),
+                        TimeUtil.toFormattedTime(contract.getEndDate()),
+                        DateUtil.toFormattedDDay(contract.getStartDate(), contract.getEndDate()),
                         contract.getTotalBalance()
                 ))
                 .toList();
 
         return new GetAllAccountsResponse(totalAccountBalance, accountResponses);
-    }
-
-    private String formatDate(LocalDate date) {
-        return date.format(FORMATTER);
-    }
-
-    private String calculateDDay(LocalDate endDate) {
-        LocalDate today = LocalDate.now();
-        long days = java.time.temporal.ChronoUnit.DAYS.between(today, endDate);
-
-        return days > 0 ? "D-" + days : "만료됨";
     }
 }
