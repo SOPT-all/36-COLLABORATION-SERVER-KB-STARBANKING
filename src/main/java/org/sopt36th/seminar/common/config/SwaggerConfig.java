3ad917d8e5de096076750b21ab8708c887d07436
kaswhy <sxshx818@naver.com>
2025-05-20 01:31:12 +0900

    [hotfix/#20] 스웨거 cors 오류 수정

--- src/main/java/org/sopt36th/seminar/common/config/SwaggerConfig.java
+++ src/main/java/org/sopt36th/seminar/common/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package org.sopt36th.seminar.common.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +13,11 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        Server server = new Server();
+        server.setUrl("https://www.mooserver.p-e.kr");
+
         return new OpenAPI()
+                .servers(List.of(server))
                 .components(new Components())
                 .info(apiInfo());
     }
